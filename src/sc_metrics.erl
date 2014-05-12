087c3776e5594965f0b4a22a3e0b599dec76c7b2
John Haugeland <stonecypher@gmail.com>
2014-05-11 21:28:20 -0700

    nines/1 and tests

--- src/sc_metrics.erl
+++ src/sc_metrics.erl
@@ -7,10 +7,20 @@
 
 -export([
 
+    nines_epsilon/0,
+
     real_nines/1,
-    real_unines/1
+    real_unines/1,
+    nines/1
 
 ]).
+
+
+
+
+nines_epsilon() ->
+
+    0.00001.
 
 
 
@@ -23,7 +33,7 @@ real_unines(X) when X == 0; X == 0.0 ->
 
 
 
-real_unines(Unavailability) ->
+real_unines(Unavailability) when Unavailability >= 0, Unavailability =< 1 ->
 
     -1 * math:log10(Unavailability).
 
@@ -31,6 +41,22 @@ real_unines(Unavailability) ->
 
 
 
-real_nines(Availability) ->
+real_nines(Availability) when Availability >= 0, Availability =< 1 ->
 
     real_unines(1 - Availability).
+
+
+
+
+
+nines(Availability) when Availability == 100; Availability == 100.0 ->
+
+    perfect;
+
+
+
+
+
+nines(Availability) when Availability >= 0, Availability =< 100 ->
+
+    trunc(real_unines(1 - (Availability / 100)) + nines_epsilon()).
--- test/sc_metrics_tests.erl
+++ test/sc_metrics_tests.erl
@@ -16,6 +16,9 @@ real_unines_test_() ->
         { "0",              ?_assert( perfect == sc_metrics:real_unines(0)              ) },
         { "0.0",            ?_assert( perfect == sc_metrics:real_unines(0.0)            ) },
 
+        { "-0",             ?_assert( perfect == sc_metrics:real_unines(-0)             ) },
+        { "-0.0",           ?_assert( perfect == sc_metrics:real_unines(-0.0)           ) },
+
         { "0.1",            ?_assert(     1.0 == sc_metrics:real_unines(0.1)            ) },
         { "0.01",           ?_assert(     2.0 == sc_metrics:real_unines(0.01)           ) },
         { "0.001",          ?_assert(     3.0 == sc_metrics:real_unines(0.001)          ) },
@@ -38,26 +41,55 @@ real_unines_test_() ->
 real_nines_test_() ->
 
     EW = fun(Test, Base) ->
-        sc_math:eq_within(Test, Base, 0.00001) % because log10's error rate grows lol-fast
+        sc_math:eq_within(Test, Base, sc_metrics:nines_epsilon()) % because log10's error rate grows lol-fast
     end,
 
     { "real_unines()", [
 
-        { "0",              ?_assert( perfect == sc_metrics:real_nines(1)              ) },
-        { "0.0",            ?_assert( perfect == sc_metrics:real_nines(1.0)            ) },
+        { "1",              ?_assert( perfect == sc_metrics:real_nines(1)              ) },
+        { "1.0",            ?_assert( perfect == sc_metrics:real_nines(1.0)            ) },
 
-        { "0.1",            ?_assert( EW(  1.0, sc_metrics:real_nines(0.9) )            ) },
-        { "0.01",           ?_assert( EW(  2.0, sc_metrics:real_nines(0.99) )           ) },
-        { "0.001",          ?_assert( EW(  3.0, sc_metrics:real_nines(0.999) )          ) },
-        { "0.0001",         ?_assert( EW(  4.0, sc_metrics:real_nines(0.9999) )         ) },
-        { "0.00001",        ?_assert( EW(  5.0, sc_metrics:real_nines(0.99999) )        ) },
-        { "0.000001",       ?_assert( EW(  6.0, sc_metrics:real_nines(0.999999) )       ) },
-        { "0.0000001",      ?_assert( EW(  7.0, sc_metrics:real_nines(0.9999999) )      ) },
-        { "0.00000001",     ?_assert( EW(  8.0, sc_metrics:real_nines(0.99999999) )     ) },
-        { "0.000000001",    ?_assert( EW(  9.0, sc_metrics:real_nines(0.999999999) )    ) },
-        { "0.0000000001",   ?_assert( EW( 10.0, sc_metrics:real_nines(0.9999999999) )   ) },
-        { "0.00000000001",  ?_assert( EW( 11.0, sc_metrics:real_nines(0.99999999999) )  ) },
-        { "0.000000000001", ?_assert( EW( 12.0, sc_metrics:real_nines(0.999999999999) ) ) }
+        { "0.9",            ?_assert( EW(  1.0, sc_metrics:real_nines(0.9) )            ) },
+        { "0.99",           ?_assert( EW(  2.0, sc_metrics:real_nines(0.99) )           ) },
+        { "0.999",          ?_assert( EW(  3.0, sc_metrics:real_nines(0.999) )          ) },
+        { "0.9999",         ?_assert( EW(  4.0, sc_metrics:real_nines(0.9999) )         ) },
+        { "0.99999",        ?_assert( EW(  5.0, sc_metrics:real_nines(0.99999) )        ) },
+        { "0.999999",       ?_assert( EW(  6.0, sc_metrics:real_nines(0.999999) )       ) },
+        { "0.9999999",      ?_assert( EW(  7.0, sc_metrics:real_nines(0.9999999) )      ) },
+        { "0.99999999",     ?_assert( EW(  8.0, sc_metrics:real_nines(0.99999999) )     ) },
+        { "0.999999999",    ?_assert( EW(  9.0, sc_metrics:real_nines(0.999999999) )    ) },
+        { "0.9999999999",   ?_assert( EW( 10.0, sc_metrics:real_nines(0.9999999999) )   ) },
+        { "0.99999999999",  ?_assert( EW( 11.0, sc_metrics:real_nines(0.99999999999) )  ) },
+        { "0.999999999999", ?_assert( EW( 12.0, sc_metrics:real_nines(0.999999999999) ) ) }
+
+    ] }.
+
+
+
+
+
+nines_test_() ->
+
+    { "nines/1", [
+
+        { "100",            ?_assert( perfect == sc_metrics:nines(100)           ) },
+        { "100.0",          ?_assert( perfect == sc_metrics:nines(100.0)         ) },
+
+        { "90",             ?_assert(       1 == sc_metrics:nines(90)            ) },
+        { "99",             ?_assert(       2 == sc_metrics:nines(99)            ) },
+
+        { "0.1",            ?_assert(       1 == sc_metrics:nines(90.0)          ) },
+        { "0.01",           ?_assert(       2 == sc_metrics:nines(99.0)          ) },
+        { "0.001",          ?_assert(       3 == sc_metrics:nines(99.9)          ) },
+        { "0.0001",         ?_assert(       4 == sc_metrics:nines(99.99)         ) },
+        { "0.00001",        ?_assert(       5 == sc_metrics:nines(99.999)        ) },
+        { "0.000001",       ?_assert(       6 == sc_metrics:nines(99.9999)       ) },
+        { "0.0000001",      ?_assert(       7 == sc_metrics:nines(99.99999)      ) },
+        { "0.00000001",     ?_assert(       8 == sc_metrics:nines(99.999999)     ) },
+        { "0.000000001",    ?_assert(       9 == sc_metrics:nines(99.9999999)    ) },
+        { "0.0000000001",   ?_assert(      10 == sc_metrics:nines(99.99999999)   ) },
+        { "0.00000000001",  ?_assert(      11 == sc_metrics:nines(99.999999999)  ) },
+        { "0.000000000001", ?_assert(      12 == sc_metrics:nines(99.9999999999) ) }
 
     ] }.
 
